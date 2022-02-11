b080b098cfe26b02a03124d025d7c58f12f4a33f
timislaw <personal.timross@gmail.com>
2022-02-11 00:47:11 -0800

    fizzbuzz

---
+++ app.js
@@ -0,0 +1,16 @@
+//fizzbuzz
+
+let answer = parseInt(prompt('Please enter the number you would like to fizzbuzz up to:'));
+
+for (let i = 1; i <= answer; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log('FizzBuzz');
+
+    } else if (i % 5 === 0) {
+        console.log('Buzz');
+    } else if (i % 3 === 0) {
+        console.log('Fizz');
+    } else {
+        console.log(i);
+    };
+};
